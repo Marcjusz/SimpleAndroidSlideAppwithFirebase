fa9d613789a8ea74d5d3238805fbeaea1081e78d
Marcjusz <ziomal38a@gmail.com>
2019-01-09 23:53:39 +0100

    early alfa version

--- app/src/main/java/com/example/mn/simpleandroidslideappwithfirebase/MainActivity.java
+++ app/src/main/java/com/example/mn/simpleandroidslideappwithfirebase/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.mn.simpleandroidslideappwithfirebase;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -36,16 +41,17 @@ public class MainActivity extends AppCompatActivity {
         readFromFirebase(database);
 
 
-        /*
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Poszedł lajk", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, AddNewContent.class);
+                startActivity(intent);
+               // Snackbar.make(view, "Poszedł lajk", Snackbar.LENGTH_LONG).setAction("Action", null).show();
             }
         });
-        */
+
     }
 
     @Override
